6969b77ff721a8c47580c6e3e757ce09979c9ed8
Jack <44051158+BJackal@users.noreply.github.com>
2020-01-14 10:03:06 +0000

    Add files via upload

---
+++ biodynamo.h
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) The BioDynaMo Project.
+// All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+
+#ifndef BIODYNAMO_H_
+#define BIODYNAMO_H_
+
+#include <string>
+
+#include "core/biology_module/biology_module.h"
+#include "core/biology_module/grow_divide.h"
+#include "core/biology_module/regulate_genes.h"
+#include "core/event/cell_division_event.h"
+#include "core/event/event.h"
+#include "core/grid.h"
+#include "core/model_initializer.h"
+#include "core/param/command_line_options.h"
+#include "core/param/param.h"
+#include "core/resource_manager.h"
+#include "core/scheduler.h"
+#include "core/shape.h"
+#include "core/sim_object/cell.h"
+#include "core/sim_object/sim_object.h"
+#include "core/util/vtune.h"
+#include "core/visualization/notebook_util.h"
+#include "core/thermal.h"
+
+#endif  // BIODYNAMO_H_
